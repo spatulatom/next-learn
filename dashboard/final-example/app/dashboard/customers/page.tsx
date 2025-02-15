02d779a06c1ea3f93e0a7df182d7c32edaeebfe3
spatulatom <spatulatom@gmail.com>
2025-02-15 21:04:45 +0000

    commnts regaring RC features on prams and searchParams being async

--- dashboard/final-example/app/dashboard/customers/page.tsx
+++ dashboard/final-example/app/dashboard/customers/page.tsx
@@ -23,3 +23,27 @@ export default async function Page(props: {
     </main>
   );
 }
+
+// Since the searchParams prop is a promise. You must use async/await or
+//  React's use function to access the values.
+// In version 14 and earlier, searchParams was a synchronous prop. To help 
+// with backwards compatibility, you can still access it synchronously in
+// Next.js 15, but this behavior will be deprecated in the future.
+// searchParams is a Dynamic API whose values cannot be known ahead of 
+// time. Using it will opt the page into dynamic rendering at request time.
+
+// export default async function Page({
+//   searchParams,
+// }: {
+//   searchParams?: { query?: string; page?: string }
+// }) {
+//   const query = searchParams?.query || '';
+
+//   const customers = await fetchFilteredCustomers(query);
+
+//   return (
+//     <main>
+//       <CustomersTable customers={customers} />
+//     </main>
+//   );
+// }
